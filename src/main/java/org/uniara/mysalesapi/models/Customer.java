9fc5f81f2bc49ebfb7a6ecf7cc455e9c4c704b37
Gustavo Coimbra <gustavocoimbra04@gmail.com>
2025-05-15 09:15:36 -0300

    Swagger config and add auto-increment to customer's id

--- src/main/java/org/uniara/mysalesapi/models/Customer.java
+++ src/main/java/org/uniara/mysalesapi/models/Customer.java
@@ -1,6 +1,8 @@
 package org.uniara.mysalesapi.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,6 +11,7 @@ import lombok.Setter;
 @Getter @Setter
 public class Customer {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String lastName;
