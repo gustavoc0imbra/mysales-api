5f98e90e118a8bbb3e64dda0631e2869a6ed7832
gustavoc0imbra <gustavocoimbra04@gmail.com>
2025-05-15 07:44:20 -0300

    Init Project

---
+++ src/main/java/org/uniara/mysalesapi/MysalesapiApplication.java
@@ -0,0 +1,13 @@
+package org.uniara.mysalesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysalesapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysalesapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/uniara/mysalesapi/MysalesapiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.uniara.mysalesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysalesapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
